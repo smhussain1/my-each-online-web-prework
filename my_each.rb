8cc256daab19336d25561863e456d1b57f607ac4
michael hussain <smhussain_1@yahoo.com>
2019-01-02 00:32:04 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,14 +1,14 @@
 
-i = 0
-numbers = []
+# i = 0
+# numbers = []
 
-while i < 6
-  print "At the top i is #{i}"
-  numbers.push(i)
+# while i < 6
+#   print "At the top i is #{i}"
+#   numbers.push(i)
 
-  i += 1
-  print "Numbers now: ", numbers
-  print "At the bottom i is #{i}"
-end
+#   i += 1
+#   print "Numbers now: ", numbers
+#   print "At the bottom i is #{i}"
+# end
 
 #puts "The numbers: "
