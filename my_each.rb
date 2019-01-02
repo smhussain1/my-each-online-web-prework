de7820ffa9fd476f21452025d763f063b1e4537e
michael hussain <smhussain_1@yahoo.com>
2019-01-02 00:26:12 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,29 +1,14 @@
-# array = []
-# i = 0
-
-# def my_each(array)
-#   while i < 6
-#   puts "i is #{i}."
-#   yields 
-#   array.push(i)
-  
-#   i += 1 
-#   puts "Numbers now:", array 
-#   end
-# end
-
-# my_each "Adding to the array."
 
 i = 0
 numbers = []
 
 while i < 6
-  puts "At the top i is #{i}"
+  return "At the top i is #{i}"
   numbers.push(i)
 
   i += 1
-  puts "Numbers now: ", numbers
-  puts "At the bottom i is #{i}"
+  return "Numbers now: ", numbers
+  return "At the bottom i is #{i}"
 end
 
 puts "The numbers: "
