9a7d3f98480210d67209e67fde0157f5f77409c6
michael hussain <smhussain_1@yahoo.com>
2019-01-02 02:33:26 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -9,15 +9,15 @@ def my_each(arr)
   array
 end
 
-def my_each(array, n)
-  array = []
-  n = 1 
-  while array.count < 8
-  array.push(n)
-  yield (array)
-  n += 1
-  end 
-end
+# def my_each(array, n)
+#   array = []
+#   n = 1 
+#   while array.count < 8
+#   array.push(n)
+#   yield (array)
+#   n += 1
+#   end 
+# end
 
 
 # my_each(array, n) do |array| array*2 end 
