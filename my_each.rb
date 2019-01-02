740d9828c57312db54753ecc808ab454bae26e64
michael hussain <smhussain_1@yahoo.com>
2019-01-02 00:24:52 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -14,3 +14,16 @@ end
 
 my_each "Adding to the array."
 
+i = 0
+numbers = []
+
+while i < 6
+  puts "At the top i is #{i}"
+  numbers.push(i)
+
+  i += 1
+  puts "Numbers now: ", numbers
+  puts "At the bottom i is #{i}"
+end
+
+puts "The numbers: "
