3f49e59d26c74ac9de4d246dde503d6a312a53c4
michael hussain <smhussain_1@yahoo.com>
2019-01-02 00:51:31 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -16,16 +16,4 @@
 # my_each { |num| puts "You see #{num}." }
 
 
-# i = 0
-# numbers = []
-
-# while i < 6
-#   print "At the top i is #{i}"
-#   numbers.push(i)
-
-#   i += 1
-#   print "Numbers now: ", numbers
-#   print "At the bottom i is #{i}"
-# end
-
-#puts "The numbers: "
+#
